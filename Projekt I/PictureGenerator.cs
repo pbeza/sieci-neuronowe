e3c5a9ecc18cb065a406a187d5e01c1771e50306
Patryk <patryk.beza@gmail.com>
2015-10-20 00:18:06 +0200

    Refactor c.d.

--- Projekt I/PictureGenerator.cs
+++ Projekt I/PictureGenerator.cs
@@ -1,24 +1,20 @@
-﻿namespace sieci_neuronowe
+﻿#region
+
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using Encog.ML;
+using Encog.ML.Data.Basic;
+using Encog.ML.Data.Versatile;
+
+#endregion
+
+namespace sieci_neuronowe
 {
-    #region
-
-    using System.Collections.Generic;
-    using System.Drawing;
-    using System.Drawing.Imaging;
-    using System.Linq;
-    using System.Runtime.InteropServices;
-
-    using Encog.ML;
-    using Encog.ML.Data;
-    using Encog.ML.Data.Basic;
-    using Encog.ML.Data.Versatile;
-
-    #endregion
-
     public static class PictureGenerator
     {
-        #region Public Methods and Operators
-
         /// <summary>
         ///     Generuje obrazek przedstawiający jak sieć dzieli przestrzeń.
         ///     Testuje tylko punkty z zakresu 0.0 - 1.0.
@@ -30,33 +26,31 @@
         /// <param name="resolutionX">Rozdzielczość w x</param>
         /// <param name="resolutionY">j.w, dla y</param>
         public static void DrawArea(
-            string path, 
-            IMLRegression testFunction, 
-            List<ClassifiedPoint> points, 
-            NormalizationHelper helper, 
-            int resolutionX, 
+            string path,
+            IMLRegression testFunction,
+            List<ClassifiedPoint> points,
+            NormalizationHelper helper,
+            int resolutionX,
             int resolutionY)
         {
             var bmp = new Bitmap(resolutionX, resolutionY);
-            BitmapData lck = bmp.LockBits(
-                new Rectangle(0, 0, resolutionX, resolutionY), 
-                ImageLockMode.WriteOnly, 
+            var lck = bmp.LockBits(new Rectangle(0, 0, resolutionX, resolutionY), ImageLockMode.WriteOnly,
                 PixelFormat.Format32bppArgb);
-            const double ResolutionMult = 3.0;
-            const double CoordOffset = -1.5;
-            double stepX = ResolutionMult / resolutionX;
-            double stepY = ResolutionMult / resolutionY;
-            for (int i = 0; i < resolutionX; i++)
+            const double resolutionMult = 3.0;
+            const double coordOffset = -1.5;
+            var stepX = resolutionMult / resolutionX;
+            var stepY = resolutionMult / resolutionY;
+            for (var i = 0; i < resolutionX; i++)
             {
-                double x = (i * stepX) + CoordOffset;
-                for (int j = 0; j < resolutionY; j++)
+                var x = (i * stepX) + coordOffset;
+                for (var j = 0; j < resolutionY; j++)
                 {
-                    double y = (j * stepY) + CoordOffset;
-                    IMLData output = testFunction.Compute(new BasicMLData(new[] { x, y }));
-                    string stringChosen = helper.DenormalizeOutputVectorToString(output)[0];
-                    int result = int.Parse(stringChosen);
-                    int colorRGB = RGBFromInt(result, points.Any());
-                    Marshal.WriteInt32(lck.Scan0 + (((i * lck.Width) + j) * 4), colorRGB);
+                    var y = (j * stepY) + coordOffset;
+                    var output = testFunction.Compute(new BasicMLData(new[] { x, y }));
+                    var stringChosen = helper.DenormalizeOutputVectorToString(output)[0];
+                    var result = int.Parse(stringChosen);
+                    var colorRGB = RGBFromInt(result, points.Any());
+                    Marshal.WriteInt32(lck.Scan0 + (i * lck.Width + j) * 4, colorRGB);
                 }
             }
 
@@ -67,15 +61,15 @@
                     continue;
                 }
 
-                double x = pt.X;
-                double y = pt.Y;
-                int colorRGB = RGBFromInt(pt.Correct, false);
+                var x = pt.X;
+                var y = pt.Y;
+                var colorRGB = RGBFromInt(pt.Correct, false);
 
-                var i = (int)((x - CoordOffset) / stepX);
-                var j = (int)((y - CoordOffset) / stepY);
+                var i = (int)((x - coordOffset) / stepX);
+                var j = (int)((y - coordOffset) / stepY);
                 if (i > 0 && i < lck.Width && j > 0 && j < lck.Height)
                 {
-                    Marshal.WriteInt32(lck.Scan0 + (((i * lck.Width) + j) * 4), colorRGB);
+                    Marshal.WriteInt32(lck.Scan0 + (i * lck.Width + j) * 4, colorRGB);
                 }
             }
 
@@ -84,44 +78,40 @@
         }
 
         public static void DrawGraph(
-            string path, 
-            IMLRegression testFunction, 
-            List<ClassifiedPoint> points, 
-            NormalizationHelper helper, 
-            int resolutionX, 
+            string path,
+            IMLRegression testFunction,
+            List<ClassifiedPoint> points,
+            NormalizationHelper helper,
+            int resolutionX,
             int resolutionY)
         {
             var bmp = new Bitmap(resolutionX, resolutionY);
-            BitmapData lck = bmp.LockBits(
-                new Rectangle(0, 0, resolutionX, resolutionY), 
-                ImageLockMode.WriteOnly, 
+            var lck = bmp.LockBits(new Rectangle(0, 0, resolutionX, resolutionY), ImageLockMode.WriteOnly,
                 PixelFormat.Format32bppArgb);
             var xmin = points.Min(p => p.X);
             var xmax = points.Max(p => p.X);
             var ymin = points.Min(p => p.Y);
             var ymax = points.Max(p => p.Y);
-            const double Padding = 1.5;
-            double dx = xmax - xmin;
-            double dy = ymax - ymin;
-            double resolutionMultX = Padding * dx;
-            double resolutionMultY = Padding * dy;
-            double stepX = resolutionMultX / resolutionX;
-            double stepY = resolutionMultY / resolutionY;
-
-            double coordOffsetX = xmin + (dx / 10);
-            double coordOffsetY = ymin + (dy / 10);
-
-            for (int i = 0; i < resolutionX; i++)
+            const double padding = 1.5;
+            double dx = xmax - xmin,
+                dy = ymax - ymin,
+                resolutionMultX = padding * dx,
+                resolutionMultY = padding * dy,
+                stepX = resolutionMultX / resolutionX,
+                stepY = resolutionMultY / resolutionY,
+                coordOffsetX = xmin + dx / 10,
+                coordOffsetY = ymin + dy / 10;
+            for (var i = 0; i < resolutionX; i++)
             {
-                double x = (i * stepX) + coordOffsetX;
-                IMLData output = testFunction.Compute(new BasicMLData(new[] { x }));
-                string stringChosen = helper.DenormalizeOutputVectorToString(output)[0];
-                double y = double.Parse(stringChosen);
-                int colorRGB = RGBFromInt(1, false);
+                var x = i * stepX + coordOffsetX;
+                var output = testFunction.Compute(new BasicMLData(new[] { x }));
+                var stringChosen = helper.DenormalizeOutputVectorToString(output)[0];
+                var y = double.Parse(stringChosen);
+                var colorRGB = RGBFromInt(1, false);
                 var j = (int)((y - coordOffsetY) / stepY);
                 if (i > 0 && i < lck.Width && j > 0 && j < lck.Height)
                 {
-                    Marshal.WriteInt32(lck.Scan0 + (((i * lck.Width) + j) * 4), colorRGB);
+                    Marshal.WriteInt32(lck.Scan0 + (i * lck.Width + j) * 4, colorRGB);
                 }
             }
 
@@ -132,9 +122,9 @@
                     continue;
                 }
 
-                double x = pt.X;
-                double y = pt.Y;
-                int colorRGB = RGBFromInt(2, false);
+                var x = pt.X;
+                var y = pt.Y;
+                var colorRGB = RGBFromInt(2, false);
 
                 var i = (int)((x - coordOffsetX) / stepX);
                 var j = (int)((y - coordOffsetY) / stepY);
@@ -148,22 +138,16 @@
             bmp.Save(path);
         }
 
-        #endregion
-
-        #region Methods
-
         private static int RGBFromInt(int i, bool lowIntensity)
         {
-            bool red = (i % 4) == 1;
-            bool green = (i % 4) == 2;
-            bool blue = (i % 4) == 3;
-            int val = lowIntensity ? 63 : 255;
-            int r = red ? val : 0;
-            int g = green ? val : 0;
-            int b = blue ? val : 0;
+            var red = (i % 4) == 1;
+            var green = (i % 4) == 2;
+            var blue = (i % 4) == 3;
+            var val = lowIntensity ? 63 : 255;
+            var r = red ? val : 0;
+            var g = green ? val : 0;
+            var b = blue ? val : 0;
             return (0x0ff << 24) | ((r & 0x0ff) << 16) | ((g & 0x0ff) << 8) | ((b & 0x0ff) << 0);
         }
-
-        #endregion
     }
 }
